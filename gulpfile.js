a4fa9967cdea8ef2fe67fdec6b4e0b5c1a3b59b9
rakuishi <rakuishi@gmail.com>
2015-03-24 22:34:02 +0900

    Use BrowserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var plumber = require('gulp-plumber');
+var browserSync = require('browser-sync');
 
 gulp.task('sass', function() {
   gulp.src('./scss/*.scss')
@@ -9,6 +10,19 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('./css'));
 });
 
-gulp.task('default', function() {
+gulp.task('browser-sync', function() {
+  browserSync({
+    server: {
+      baseDir: "./"
+    }
+  });
+});
+
+gulp.task('bs-reload', function () {
+  browserSync.reload();
+});
+
+gulp.task('default', ['browser-sync'], function() {
   gulp.watch('./scss/*.scss', ['sass']);
+  gulp.watch(['./*.html', './css/style.css'], ['bs-reload']);
 });
